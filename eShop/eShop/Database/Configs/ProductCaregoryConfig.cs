b42d2121bb1b3365ff6b3001dd444328370eae3d
ThuyTien2203 <tranthithuytien2203@gmail.com>
2022-08-24 19:51:59 +0700

    upload code

---
+++ eShop/eShop/Database/Configs/ProductCaregoryConfig.cs
@@ -0,0 +1,16 @@
+﻿using eShop.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eShop.Database.Configs
+{
+	public class ProductCaregoryConfig : IEntityTypeConfiguration<ProductCategory>
+	{
+		public void Configure(EntityTypeBuilder<ProductCategory> builder)
+		{
+			builder.Property(x => x.Name).HasMaxLength(200);
+		}
+	}
+
+	
+}
---
+++ eShop/eShop/Database/Entities/Base/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace eShop.Database.Entities.Base
+{
+	public class BaseEntity
+	{
+		public int Id { get; set; }
+
+		public DateTime? CreatedAt { get; set; }
+
+		public DateTime? UpdateAt { get; set; }
+
+		public DateTime? DeletedAt { get; set; }
+
+		public int? UpdateUserId { get; set; }
+
+		public int? CreateUserId { get; set; }
+
+	}
+}
