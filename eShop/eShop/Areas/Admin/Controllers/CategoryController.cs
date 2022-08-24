5aa183ce5d696ffe0471bc2fa250e6cd07cfebf7
ThuyTien2203 <tranthithuytien2203@gmail.com>
2022-08-24 21:29:10 +0700

    eshop_2

---
+++ eShop/eShop/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using eShop.Database;
+using eShop.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Areas.Admin.Controllers
+{
+	public class CategoryController : BaseController
+	{
+		public CategoryController(AppDbContext db) : base(db)
+		{ 
+		}
+		public IActionResult Index()
+		{
+			var data = _db.ProductCategories.OrderByDescending(c => c.Id).ToList();
+
+			return View(data);
+		}
+
+		public IActionResult Create() => View();
+
+		[HttpPost]
+		public IActionResult Create(ProductCategory category)
+		{
+			category.CreatedAt = DateTime.Now;
+			category.UpdateAt = DateTime.Now;	
+			_db.ProductCategories.Add(category);
+			_db.SaveChanges();	
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
